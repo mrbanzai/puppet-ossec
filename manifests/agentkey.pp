dac71332be5a9b708d4596f862c803e7ad53522e
Michael Porter <michael.porter@ingramcontent.com>
2015-09-16 14:20:05 -0400

    Utilize common params class, and add Windows support

--- manifests/agentkey.pp
+++ manifests/agentkey.pp
@@ -5,13 +5,15 @@ define ossec::agentkey(
   $agent_ip_address,
   $agent_seed = 'xaeS7ahf',
 ) {
+  require ossec::params
+
   if ! $agent_id { fail("ossec::agentkey: ${agent_id} is missing")}
 
   $agentKey1 = md5("${agent_id} ${agent_seed}")
   $agentKey2 = md5("${agent_name} ${agent_ip_address} ${agent_seed}")
 
   concat::fragment { "var_ossec_etc_client.keys_${agent_name}_part":
-    target  => '/var/ossec/etc/client.keys',
+    target  => $ossec::params::keysfile,
     order   => $agent_id,
     content => "${agent_id} ${agent_name} ${agent_ip_address} ${agentKey1}${agentKey2}\n",
   }
--- manifests/server.pp
+++ manifests/server.pp
@@ -13,7 +13,7 @@ class ossec::server (
   $ossec_check_frequency               = 79200,
   $use_mysql                           = false,
   $manage_repos                        = false
-) {
+) inherits ossec::params {
   validate_bool(
     $ossec_active_response, $ossec_rootcheck,
     $use_mysql, $manage_repos
@@ -23,93 +23,33 @@ class ossec::server (
   #validate_integer($ossec_check_frequency, undef, 1800)
   validate_array($ossec_ignorepaths)
 
-  class { 'ossec::packages':
-    manage_repos => $manage_repos
+  if $::osfamily == 'windows' {
+    fail('The ossec module does not yet support installing the OSSEC HIDS server on Windows')
+  }
+
+  if $manage_repos {
+    # TODO: Allow filtering of EPEL requirement
+    include ossec::repo
+    Class['ossec::repo'] -> Package[$ossec::params::server_package]
   }
 
   if $use_mysql {
+    # Relies on mysql module specified in metadata.json
     include mysql::client
+    Class['mysql::client'] ~> Service[$ossec::params::server_service]
   }
 
   # install package
-  case $::osfamily {
-    'Debian' : {
-      $ossec_local_files = {
-        '/var/log/syslog'             => 'syslog',
-        '/var/log/auth.log'           => 'syslog',
-        '/var/log/mail.log'           => 'syslog',
-        '/var/log/dpkg.log'           => 'syslog',
-        '/var/log/apache2/access.log' => 'apache',
-        '/var/log/apache2/error.log'  => 'apache'
-      }
-      package { $ossec::packages::hidsserverpackage:
-        ensure  => installed,
-        require => Apt::Source['alienvault'],
-      }
-    }
-    'RedHat' : {
-      $ossec_local_files = {
-        '/var/log/messages'         => 'syslog',
-        '/var/log/secure'           => 'syslog',
-        '/var/log/maillog'          => 'syslog',
-        '/var/log/yum.log'          => 'syslog',
-        '/var/log/httpd/access_log' => 'apache',
-        '/var/log/httpd/error_log'  => 'apache'
-      }
-      case $::operatingsystem {
-        'RedHat', 'CentOS', 'OracleLinux' : {
-          case $::operatingsystemmajrelease {
-            '7' : {
-              if $use_mysql {
-                package { 'mariadb': ensure => present }
-                package { 'ossec-hids':
-                  ensure   => installed,
-                }
-                package { $ossec::packages::hidsserverpackage:
-                  ensure  => installed,
-                  require => Package['mariadb'],
-                }
-              } else {
-                package { 'ossec-hids':
-                  ensure   => installed,
-                }
-                package { $ossec::packages::hidsserverpackage:
-                  ensure  => installed,
-                }
-              }
-            }
-            default: {
-              if $use_mysql {
-                package { 'mysql': ensure => present }
-                package { 'ossec-hids':
-                  ensure   => installed,
-                }
-                package { $ossec::packages::hidsserverpackage:
-                  ensure  => installed,
-                  require => Package['mysql'],
-                }
-              } else {
-                package { 'ossec-hids':
-                  ensure   => installed,
-                }
-                package { $ossec::packages::hidsserverpackage:
-                  ensure  => installed,
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    default: { fail('OS family not supported') }
+  package { $ossec::params::server_package:
+    ensure  => installed
   }
 
-  service { $ossec::packages::hidsserverservice:
+  service { $ossec::params::server_service:
     ensure    => running,
     enable    => true,
-    hasstatus => $ossec::packages::servicehasstatus,
-    pattern   => $ossec::packages::hidsserverservice,
-    require   => Package[$ossec::packages::hidsserverpackage],
+    hasstatus => $ossec::params::service_has_status,
+    pattern   => $ossec::params::server_service,
+    require   => Package[$ossec::params::server_package],
   }
 
   # configure ossec
@@ -117,35 +57,36 @@ class ossec::server (
     owner   => 'root',
     group   => 'ossec',
     mode    => '0440',
-    require => Package[$ossec::packages::hidsserverpackage],
-    notify  => Service[$ossec::packages::hidsserverservice]
+    require => Package[$ossec::params::server_package],
+    notify  => Service[$ossec::params::server_service]
   }
   concat::fragment { 'ossec.conf_10' :
     target  => '/var/ossec/etc/ossec.conf',
     content => template('ossec/10_ossec.conf.erb'),
     order   => 10,
-    notify  => Service[$ossec::packages::hidsserverservice]
+    notify  => Service[$ossec::params::server_service]
   }
   concat::fragment { 'ossec.conf_90' :
     target  => '/var/ossec/etc/ossec.conf',
     content => template('ossec/90_ossec.conf.erb'),
     order   => 90,
-    notify  => Service[$ossec::packages::hidsserverservice]
+    notify  => Service[$ossec::params::server_service]
   }
 
   concat { '/var/ossec/etc/client.keys':
     owner   => 'root',
     group   => 'ossec',
     mode    => '0640',
-    notify  => Service[$ossec::packages::hidsserverservice],
-    require => Package[$ossec::packages::hidsserverpackage],
+    notify  => Service[$ossec::params::server_service],
+    require => Package[$ossec::params::server_package],
   }
   concat::fragment { 'var_ossec_etc_client.keys_end' :
     target  => '/var/ossec/etc/client.keys',
     order   => 99,
     content => "\n",
-    notify  => Service[$ossec::packages::hidsserverservice]
+    notify  => Service[$ossec::params::server_service]
   }
+
   Ossec::Agentkey<<| |>>
 
 }
